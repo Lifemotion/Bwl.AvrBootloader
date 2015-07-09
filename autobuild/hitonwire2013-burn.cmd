ddd758e28a496e4c74ce3be1a68d345c0f437001
Igor Koshelev <igor@lifemotion.ru>
2015-07-09 10:23:47 +0300

    Загрузчик для HW2013

---
+++ autobuild/hitonwire2013-burn.cmd
@@ -0,0 +1,3 @@
+call paths.cmd
+atprogram -t jtagice3 -i ISP -d atmega88PA chiperase program -fl -fs -f boot_hw2013
+.elf
---
+++ autobuild/hitonwire2013-compile.cmd
@@ -0,0 +1,4 @@
+set CFG_NAME=CFG_HITONWIRE_2013
+set FILENAME=boot_hw2013
+
+start compile-stub.cmd
