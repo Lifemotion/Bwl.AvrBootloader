3541fd862779cf024efe920c51266437118102f3
Andrew <Andrew@DESKTOP-59JS694>
2016-05-23 16:30:14 +0300

    Добавлены команды для проишвки разных МК.

---
+++ autobuild/mega324p-16mhz.cmd
@@ -0,0 +1,3 @@
+call mega324p-generic-compile-16mhz.cmd
+call mega324p-generic-burn-16mhz.cmd
+pause
---
+++ autobuild/mega324p-generic-compile-16mhz.cmd
@@ -0,0 +1,4 @@
+set CFG_NAME=CFG_M324_GENERIC_16MHz
+set FILENAME=boot_m324p_16mhz
+
+call compile-stub.cmd
